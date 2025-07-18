7cff0a000a544abcb404c268250c405f543facef
MRaysa <147266032+MRaysa@users.noreply.github.com>
2025-07-18 23:10:36 +0600

    added user and admin route

---
+++ routes/adminRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const {
+  getAllArticles,
+  approveArticle,
+  declineArticle,
+  makeArticlePremium,
+  getAllUsers,
+  promoteToAdmin,
+  addPublisher,
+  calculateArticleDistribution,
+} = require("../controller/admin.controller");
+const { deleteArticle } = require("../controller/article.controller");
+
+const router = express.Router();
+
+router.get("/articles", getAllArticles);
+router.patch("/articles/approve/:id", approveArticle);
+router.patch("/articles/decline/:id", declineArticle);
+router.delete("/articles/delete/:id", deleteArticle);
+router.patch("/articles/premium/:id", makeArticlePremium);
+router.get("/all-users", getAllUsers);
+router.patch("/users/admin/:id", promoteToAdmin);
+router.post("/admin/add-publisher", addPublisher);
+router.get("/calculate/article/distribution", calculateArticleDistribution);
+
+module.exports = router;
