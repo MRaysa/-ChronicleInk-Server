fef612168fd463da20e5b4d14433e28df739ba3c
MRaysa <147266032+MRaysa@users.noreply.github.com>
2025-07-19 01:03:24 +0600

    jwt intregrate

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,14 +1,22 @@
 const admin = require("firebase-admin");
 require("dotenv").config();
 
-const fileData = Buffer.from(
-  process.env.FIREBASE_SERVICES_KEY,
-  "base64"
-).toString("utf8");
-const serviceAccount = JSON.parse(fileData);
+try {
+  const fileData = Buffer.from(
+    process.env.FIREBASE_SERVICES_KEY,
+    "base64"
+  ).toString("utf8");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+  const serviceAccount = JSON.parse(fileData);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+
+  console.log("Firebase Admin SDK initialized successfully.");
+} catch (error) {
+  console.error("Error initializing Firebase Admin SDK:", error.message);
+  // process.exit(1);
+}
 
 module.exports = admin;
